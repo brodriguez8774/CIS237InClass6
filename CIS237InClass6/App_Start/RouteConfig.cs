116814894f4191c44132ab94d3f2f97e75a0e479
Brandon Rodriguez <brodriguez8774@mail.kvcc.edu>
2015-12-08 15:54:51 -0500

    First commit

---
+++ CIS237InClass6/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CIS237InClass6
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Route we made.
+            routes.MapRoute(
+                name: "Mine",
+                url: "my/own/route/", // What you type in.
+                defaults: new { controller = "Cars", action = "Index" } // What the site links to.
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            
+            
+             
+        }
+    }
+}
---
+++ CIS237InClass6/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CIS237InClass6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            // More testing of how default routes work.
+            // ViewBag is a way to send data to the view.
+            ViewBag.SomethingElse = "Something Else Here";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        // Added method to test how default route works.
+        // Will get called if browser is pointed to /home/foo
+        public ActionResult Foo()
+        {
+            // Content is a calss that implements ActionResult so we can return it.
+            // Simply returns some text. Nothing fancy.
+            return View();
+        }
+    }
+}
