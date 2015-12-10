a1467576fae4ac2511c6414ce00d96fb8ed83251
Brandon Rodriguez <brodriguez8774@mail.kvcc.edu>
2015-12-10 15:57:10 -0500

    Add basic javascript foundations

---
+++ CIS237InClass6/Scripts/cars.js
@@ -0,0 +1,11 @@
+﻿
+// Make sure document is loaded before doing anything. Almost always first, with rest of code nested inside.
+$(document).ready(function () {
+    // This is a jQuery selector. Sets onclick listener.
+    $(".dataRow").click(function (firedEvent) {
+        console.log("The event fired.");
+        // document.location = "/cars/edit/*idhere*"
+    });
+});
+
+
